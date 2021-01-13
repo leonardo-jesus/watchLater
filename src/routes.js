f23108a8336f82ae046ee37e046db80a24aaefc0
Leonardo <leonardojesus02@outlook.com>
2021-01-13 11:39:28 -0300

    feat: Add new routes and cron

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
+const cron = require('node-cron');
 const ListCards = require('./controllers/ListCards');
+const CreateCard = require('./controllers/CreateCard');
+const SearchVideos = require('./controllers/SearchVideos');
 
 router.get('/cards', ListCards.index);
+router.post('/cards', CreateCard.create);
+router.get('/videos', SearchVideos.index);
+
+cron.schedule("* * * * *", () => {
+    SearchVideos.index();
+
+    console.log("✔️  CRON ACTIVATED");
+});
+
+// cron.schedule("0 */1 * * *", () => {
+//     SearchVideos.index();
+//
+//     console.log("✔️ CRON ACTIVATED");
+// });
 
 module.exports = router;
